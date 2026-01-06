5ab558171b0610124102f30d96969f0f8ff2fd3b
pastor <142336369+PastorL69@users.noreply.github.com>
2026-01-07 00:26:14 +0100

    Change SPI bits per word from 16 to 8

--- src/ZeDMDSpi.cpp
+++ src/ZeDMDSpi.cpp
@@ -16,7 +16,7 @@
 
 namespace
 {
-constexpr uint8_t kSpiBitsPerWord = 16;
+constexpr uint8_t kSpiBitsPerWord = 8;
 constexpr uint8_t kSpiMode = SPI_MODE_0;
 constexpr unsigned int kCsGpio = 25;  // GPIO25 on Raspberry Pi
 constexpr const char kGpioConsumer[] = "ZeDMDSpi";
